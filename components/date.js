fc65afc54fac67cdb5523316f0b98df781a7965b
Naman Bhatia <bhatia.naman@hotmail.com>
2022-01-24 15:21:22 +0530

    styled component

--- components/date.js
+++ components/date.js
@@ -1,6 +1,6 @@
 import { parseISO, format } from 'date-fns'
 
-export default function Date({ dateString }) {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+export default function Date({ dateform }) {
+  const date = parseISO(dateform)
+  return <time dateTime={dateform}>{format(date, 'LLLL d, yyyy')}</time>
 }
